d9478cb910700d7e0ace5e601da798f8ca497814
Maciej Ratajczak <mratajczak@p29-10.labpk.inf.ug.edu.pl>
2016-11-05 10:42:27 +0100

    kolejna wiadomosc

--- program.c
+++ program.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 
 int main() {
-  float counter;
-  scanf("%f",&counter);
-  printf("%f\n",counter);
-  int x;
-  if (x%2==0) && (x%3==0) && (x%5==0) {
-    printf("dzieli się przez 2,3 i 5\n");
-  } else if (x%2==0) && (x%3==0) {
-    printf("dzieli się przez 2,3\n");
-  } else if (x%2==0) && (x%5==0) {
-    printf("dzieli się przez 2,5\n");
-  } else if (x%3==0) && (x%5==0) {
-    printf("dzieli się przez 3,5\n");
-  } else if (x%2==0) {
-    printf("dzieli się przez 2\n");
-  } else if (x%3==0) {
-    printf("dzieli się przez 3\n");
-  } else if (x%5==0) {
-    printf("dzieli się przez 5\n");
-  } else {
-    printf("nie dzieli się przez 2,3 i 5\n", );
+  int a;
+  scanf("%d",&a);
+  int b;
+  scanf("%d",&b);
+  int c;
+  scanf("%d",&c);
+    double wynik = (a*b)-(b/(double)c);
+    int wynik2 = (a*b)-(b%c);
+    int wynik3 = (a*b)-(c*a)-(b*c);
+printf ("%f\n",wynik);
+printf ("%d\n",wynik2);
+printf ("%d\n",wynik3);
+if ((a>b) && (b>c)) {
+  printf("%d, %d, %d\n",a,b,c );
+} else if ((c<b) && (b<a)) {
+  printf("%d, %d, %d\n",c,b,a );
+} else if ((a<c) && (c<b)) {
+  printf("%d, %d, %d\n",a,c,b );
+} else if ((b<c) && (c<a)) {
+  printf("%d, %d, %d\n",b,c,a );
+} else if ((b<a) && (a<c)) {
+  printf("%d, %d, %d\n",b,a,c );
+} else if ((c<a) && (a<b)) {
+  printf("%d, %d, %d\n",c,a,b );
 
-  return 0;
   }
+    return 0;
+}
