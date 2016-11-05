2d1ab6953ed80a388e3fb10b2c418cad6b9822ed
Maciej Ratajczak <mratajczak@p29-10.labpk.inf.ug.edu.pl>
2016-11-05 10:37:09 +0100

    jakas wiadomosc

--- program2.c
+++ program2.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main() {
-  float counter;
-  scanf("%f",&counter);
-  printf("%f\n",counter);
   int x;
-  if (x%2==0) && (x%3==0) && (x%5==0) {
+  scanf("%d",&x);
+  printf("%d\n",x);
+  if (x%2==0 && x%3==0 && x%5==0) {
     printf("dzieli się przez 2,3 i 5\n");
-  } else if (x%2==0) && (x%3==0) {
+  } else if (x%2==0 && x%3==0) {
     printf("dzieli się przez 2,3\n");
-  } else if (x%2==0) && (x%5==0) {
+  } else if (x%2==0 && x%5==0) {
     printf("dzieli się przez 2,5\n");
-  } else if (x%3==0) && (x%5==0) {
+  } else if (x%3==0 && x%5==0) {
     printf("dzieli się przez 3,5\n");
   } else if (x%2==0) {
     printf("dzieli się przez 2\n");
@@ -20,7 +19,8 @@ int main() {
   } else if (x%5==0) {
     printf("dzieli się przez 5\n");
   } else {
-    printf("nie dzieli się przez 2,3 i 5\n", );
+    printf("nie dzieli się przez 2,3 i 5\n");
+  }
 
   return 0;
   }
